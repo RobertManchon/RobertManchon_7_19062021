19c50f4195ca0c9f46961f3e8190e2ed59d073bb
erasme666 <erasme666@gmail.com>
2021-07-12 10:25:57 +0200

    quicksort more functions added

--- script.js
+++ script.js
@@ -123,3 +123,65 @@ let partition = (array, left, right) => {
     }
     return left;
 }
+//filter and extract only id, name, ingredients, and description
+let createFilteredArr = (arr) => {
+    let filteredArr = [];
+    for (let i = 0; i<arr.length; i++) {
+        let filtered = (({id, ingredients, name, description}) => ({id, ingredients, name, description}))(arr[i][1]);
+        filteredArr.push(filtered);
+    }
+    return filteredArr;
+}
+let filteredArr = createFilteredArr(recipesArray);
+
+function FilterKeyword(item) {
+    this.id = item.id;
+    let thisIngre = item.ingredients.map(b => b.ingredient.toLowerCase()).flat();
+    let keywordString = item.name + " " + thisIngre + " " + item.description;
+    let uniqueValue = [...new Set(keywordString.split(/[\s,().]+/))];
+    this.keyword = quickSort(uniqueValue, 0, uniqueValue.length-1);
+}
+
+let extractKeyword = (arr) => {
+    let newArr = [];
+    for (let i=0; i<arr.length; i++) {
+        let keyword = new FilterKeyword(arr[i]);
+        newArr.push(keyword);
+    }
+    return newArr;
+}
+let filteredKeywordArr = extractKeyword(filteredArr);
+console.log(filteredKeywordArr);
+//take only the keywords
+let allKeywords = [];
+filteredKeywordArr.forEach(item => {allKeywords.push(item.keyword)});
+let flatKeyword = allKeywords.flat();
+let allKeywordsLowerCase = [];
+flatKeyword.forEach(word => {allKeywordsLowerCase.push(word.toLowerCase())});
+let searchOptionsNotSorted = [...new Set(allKeywordsLowerCase.flat())];
+//sort by alphabetical order
+let searchOptions = quickSort(searchOptionsNotSorted, 0, searchOptionsNotSorted.length-1);
+console.log(searchOptions);
+
+function KeywordObject(item) {
+    this.keyword = item;
+    let recipeIds = [];
+    for (let i=0; i<filteredKeywordArr.length; i++) {
+        if (filteredKeywordArr[i].keyword.indexOf(item) >= 0 ) {
+            recipeIds.push(filteredKeywordArr[i].id);
+        }
+    }
+    this.ids = recipeIds;
+}
+
+let keywordArray = (arr) => {
+    let newArr = [];
+    for (let i=0; i<arr.length; i++) {
+        let keyword = new KeywordObject(arr[i]);
+        newArr.push(keyword);
+    }
+    return newArr;
+}
+
+let keywordObjectArray = keywordArray(searchOptions);
+console.log(keywordObjectArray);
